a295c0406b15b5a5783fb4907db15666ba99d81f
Harlan Williams <hrw@ku.edu>
2024-03-28 14:48:51 -0500

    Complete easy tests

--- easy/occurrence_data.R
+++ easy/occurrence_data.R
@@ -1,11 +1,27 @@
 library(tidyverse)
 library(spocc)
 
-data <- occ('Canis lupus', from='gbif', limit=100)
+data <- occ('Canis lupus', from='gbif', limit=50)
 gbif_data <- data$gbif
 canis_lupus <- gbif_data$data$Canis_lupus
 
-filtered <- canis_lupus %>%
-  filter(!is.null(longitude) && !is.null(longitude)) %>%
-  filter(coordinateUncertaintyInMeters < 1000) %>%
-  select(c(species, longitude, latitude, day, month, year))
+print('Number of records after download:')
+print(nrow(canis_lupus))
+
+filtered <- canis_lupus %>% filter(!is.null(longitude) && !is.null(longitude))
+
+print('Number of records after filtering out null geographic coordinates:')
+print(nrow(filtered))
+
+filtered <- filtered %>% filter(coordinateUncertaintyInMeters <= 1000)
+
+print('Number of records after filtering out coordinate uncertainty > 1000:')
+print(nrow(filtered))
+
+filtered <- filtered  %>% select(c(species, longitude, latitude, day, month, year))
+
+print('Number of records after selecting only species, longitude, latitude, day, month, year:')
+print(nrow(filtered))
+
+print('Dataframe after allperforming all filter and select operations')
+print(filtered, n = 50)
