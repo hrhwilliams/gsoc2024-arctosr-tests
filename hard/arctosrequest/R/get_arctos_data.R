eaaaca79f75d44791d74ca572ac5d917413bf00a
Harlan Williams <hrw@ku.edu>
2024-03-29 21:39:59 -0500

    Documentation test

--- hard/arctosrequest/R/get_arctos_data.R
+++ hard/arctosrequest/R/get_arctos_data.R
@@ -1,3 +1,13 @@
+#'get_arctos_data
+#'
+#'Returns a dataframe of ten records for a
+#'
+#' @param scientific_name Scientific or Latin name for a species
+#' @param api_key Your API key used to access Arctos
+#' @returns dataframe
 get_arctos_data <- function(scientific_name, api_key) {
-  catalog_request(list(`scientific_name`=scientific_name), api_key)
+  resp <- catalog_request(list(`scientific_name`=scientific_name), api_key)
+  d = resp$DATA
+  data.frame(matrix(unlist(d), nrow = length(d), byrow = TRUE)) |>
+    setNames(names(d[[1]]))
 }
